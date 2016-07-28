2754f3aa62012e7ee04a31135071790046b22b89
willsonbin <willsonbin6@gmail.com>
2016-07-28 16:25:14 +0800

    notice

---
+++ src/main/java/org/sdgas/model/Notice.java
@@ -0,0 +1,19 @@
+package org.sdgas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by 120378 on 2016/7/28.
+ */
+@Entity
+@Table(name = "bbs")
+public class Notice {
+
+    /**
+     * 公告编号
+     */
+    private int noticeId;
+
+
+}
